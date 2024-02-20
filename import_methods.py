01dcc3d0bbfb3664836b7efbf9341c1f34e41182
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-02-20 09:26:30 -0800

    amended get_argo()

--- import_methods.py
+++ import_methods.py
@@ -4,6 +4,7 @@ import argopy as argo
 def get_argo() :
     '''
     currently just implements the first code in the argo docs to make sure it works
+    :param : None
     '''
     ds = argo.DataFetcher().region([-75, -45, 20, 30, 0, 100, '2011-01', '2011-06']).to_xarray()
 
