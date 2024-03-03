e69cdb62ee1a82d4bd252bcb5959a47038c16713
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-03-02 17:30:40 -0800

    editted global_temp.py

--- global_temp.py
+++ global_temp.py
@@ -1,47 +1,61 @@
 from argopy import ArgoIndex  #  This is the class to work with Argo index content
 from argopy import ArgoNVSReferenceTables  # This is the class to retrieve data from Argo reference tables
 from argopy.plot import scatter_map  # This is a function to easily make maps 
-
+import argopy
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load an index of BGC profiles:
-# idx = ArgoIndex(index_file='bgc-s').load()  # 'bgc-s' is a shortcut for 'argo_synthetic-profile_index.txt'
-idx = ArgoIndex(index_file='bgc-b').load()  # 'bgc-b' is a shortcut for 'argo_bio-profile_index.txt'
 
-# Define a parameter to work with:
-# param = 'BBP470'
-# param = 'DOXY'
-param = 'BBP700'
-# Get more verbose information about this parameter (usefull for plot titles):
-reftbl = ArgoNVSReferenceTables().tbl('R03')
-param_info = reftbl[reftbl['altLabel']==param].iloc[0]
+if __name__ == "__main__" :
 
-# Search for this parameter profiles:
-idx.search_params(param)
+    argopy.set_options(src='erddap', dataset='phy', mode='standard')
 
-# Export search results to a dataframe:
-df = idx.to_dataframe()
+    ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 10, '2011-01', '2011-06'])
+    ds = ArgoSet.data.argo.point2profile().to_dataframe()
+    df = ArgoSet.index
 
-# To make the scatter map, we need to have the data mode available in one DataFrame column
-# so we need to add a new column with the DATA_MODE of the PARAMETER:
-df["variables"] = df["parameters"].apply(lambda x: x.split())
-df["%s_DM" % param] = df.apply(lambda x: x['parameter_data_mode'][x['variables'].index(param)] if param in x['variables'] else '', axis=1)
+    # Define a parameter to work with:
+    param = 'TEMP'
+    # print(len(ds[param]))
+    fig, ax = scatter_map(ds,
+                          hue=param,
+                          figsize=(10,6),
+                          set_global=True,
+                          traj=False,
+                          markersize=2,
+                          markeredgecolor=None,
+                          legend_title='Temp'
+                   )
+    plt.show()
 
 
-# Finally plot the map:
-fig, ax = scatter_map(df,
-                        hue="%s_DM" % param,
-                        cmap="data_mode",
-                        figsize=(10,6),
-                        markersize=2,
-                        markeredgecolor=None,
-                        traj=False,  # Because some floats do weird things around 180/-180
-                        set_global=False,
-                        legend_title='%s data mode' % param)
-ax.set_title("title" % (param_info['prefLabel'], 
-                                                                   param, 
-                                                                   "{:,}".format(idx.N_MATCH), 
-                                                                   idx.convention_title)
-            )
-plt.show()
+# # Get more verbose information about this parameter (usefull for plot titles):
+# reftbl = ArgoNVSReferenceTables().tbl('R03')
+# param_info = reftbl[reftbl['altLabel']==param].iloc[0]
+
+# # Export search results to a dataframe:
+# df = idx.to_dataframe()
+
+# # To make the scatter map, we need to have the data mode available in one DataFrame column
+# # so we need to add a new column with the DATA_MODE of the PARAMETER:
+# df["variables"] = df["parameters"].apply(lambda x: x.split())
+# df["%s_DM" % param] = df.apply(lambda x: x['parameter_data_mode'][x['variables'].index(param)] if param in x['variables'] else '', axis=1)
+
+
+# # Finally plot the map:
+# fig, ax = scatter_map(df,
+#                       hue="%s_DM" % param,
+#                       cmap="data_mode",
+#                       figsize=(10,6),
+#                       markersize=2,
+#                       markeredgecolor=None,
+#                       traj=False,
+#                       set_global=False,
+#                       legend_title='%s data mode' % param
+#                     )
+# ax.set_title("Data mode for '%s' (%s)\n%s profiles from the %s" % (param_info['prefLabel'], 
+#                                                                    param, 
+#                                                                    "{:,}", 
+#                                                                    idx.convention_title)
+#             )
+# plt.show()
