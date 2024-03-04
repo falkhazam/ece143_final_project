d04f08766948ac6aea171de12a9dfa9fae10a404
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-03-03 19:01:13 -0800

    gitignore added for data and plots

--- global_temp.py
+++ global_temp.py
@@ -11,19 +11,26 @@ if __name__ == "__main__" :
 
     argopy.set_options(src='erddap', dataset='phy', mode='standard')
 
-    print("Importing data...")
-    t0 = time.time()
-    ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01', '2015-02'])
-    print("Time to import : ", time.time()-t0)
-    ds = ArgoSet.data.argo.point2profile().to_dataframe()
-    print("Time to get data from DataFetcher : ", time.time()-t0)
+    
+
+    # if reading data for the first time, uncomment the following 7 lines :
+    # print("Importing data...")
+    # t0 = time.time()
+    # ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01', '2015-02'])
+    # print("Time to import : ", time.time()-t0)
+    # ds = ArgoSet.data.argo.point2profile().to_dataframe()
+    # print("Time to get data from DataFetcher : ", time.time()-t0)
+    # ds.to_csv('data.csv') 
+
+    # if getting the data from a csv file, uncomment the following 1 line :
+    df = pd.read_csv('data.csv')
 
     # Define a parameters to work with:
     param = 'TEMP'
     lat = 'LATITUDE'
     long = 'LONGITUDE'
 
-    print("data available: ", ds.columns)
+    print("data available: ", df.columns)
 
     print("Plotting data...")
 
@@ -33,7 +40,7 @@ if __name__ == "__main__" :
 
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.coastlines()
-    ax.scatter(list(ds[long]), list(ds[lat]), c=list(ds[param]))
+    ax.scatter(list(df[long]), list(df[lat]), c=list(df[param]))
     
-    plt.savefig('coastlines.png')
+    plt.savefig('plot.png')
     plt.show()
