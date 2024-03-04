bc0190b282acb8f9dace2003f08f4c7fae743c0f
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-03-03 20:05:04 -0800

    plots temp

--- global_temp.py
+++ global_temp.py
@@ -11,8 +11,6 @@ if __name__ == "__main__" :
 
     argopy.set_options(src='erddap', dataset='phy', mode='standard')
 
-    
-
     # if reading data for the first time, uncomment the following 7 lines :
     # print("Importing data...")
     # t0 = time.time()
@@ -25,22 +23,28 @@ if __name__ == "__main__" :
     # if getting the data from a csv file, uncomment the following 1 line :
     df = pd.read_csv('data.csv')
 
-    # Define a parameters to work with:
-    param = 'TEMP'
-    lat = 'LATITUDE'
-    long = 'LONGITUDE'
+    # Define the variables to work with:
+    temp = np.array(df['TEMP'])
+    lat = np.array(df['LATITUDE'])
+    long = np.array(df['LONGITUDE'])
 
-    print("data available: ", df.columns)
+    # print("data available: ", df.columns)
 
-    print("Plotting data...")
+    # print("Plotting data...")
 
-    # print(list(ds[long]))
-    # print(ds[lat])
-    # print(ds[param])
-
+    fig = plt.figure()
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.coastlines()
-    ax.scatter(list(df[long]), list(df[lat]), c=list(df[param]))
+    global_temp = ax.scatter(long, lat, c=temp)
+
+    # levels = np.linspace(temp.min(), temp.max(), 20)
+    # global_temp = ax.contourf(long, lat, temp, levels=levels)
+
+    # title and label :
+    cbar = fig.colorbar(global_temp, location='bottom')
+    cbar.set_label('Celcius')
+    ax.set_title('Sea Surface Temperature')
+    gridlines = ax.gridlines(draw_labels=True)
     
     plt.savefig('plot.png')
     plt.show()
