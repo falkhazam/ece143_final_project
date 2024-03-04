ee98c93e610583b6833cc12b95b3fd8fc9881a22
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-03-03 18:47:43 -0800

    plots temp

--- global_temp.py
+++ global_temp.py
@@ -2,6 +2,9 @@ import argopy
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import time
+import cartopy.crs as ccrs
+
 
 
 if __name__ == "__main__" :
@@ -9,55 +12,28 @@ if __name__ == "__main__" :
     argopy.set_options(src='erddap', dataset='phy', mode='standard')
 
     print("Importing data...")
-
-    ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01-01', '2015-01-10'])
+    t0 = time.time()
+    ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01', '2015-02'])
+    print("Time to import : ", time.time()-t0)
     ds = ArgoSet.data.argo.point2profile().to_dataframe()
-    df = ArgoSet.index
+    print("Time to get data from DataFetcher : ", time.time()-t0)
 
-    print("Finished importing data!")
-
-    # Define a parameter to work with:
+    # Define a parameters to work with:
     param = 'TEMP'
+    lat = 'LATITUDE'
+    long = 'LONGITUDE'
+
+    print("data available: ", ds.columns)
+
+    print("Plotting data...")
+
+    # print(list(ds[long]))
+    # print(ds[lat])
+    # print(ds[param])
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+    ax.scatter(list(ds[long]), list(ds[lat]), c=list(ds[param]))
     
-    fig, ax = argopy.plot.scatter_map(ds,
-                                      hue=param,
-                                      figsize=(10,6),
-                                      set_global=True,
-                                      traj=False,
-                                      markersize=2,
-                                      markeredgecolor=None,
-                                      legend_title='Temp'
-                                    )
+    plt.savefig('coastlines.png')
     plt.show()
-
-
-# # Get more verbose information about this parameter (usefull for plot titles):
-# reftbl = ArgoNVSReferenceTables().tbl('R03')
-# param_info = reftbl[reftbl['altLabel']==param].iloc[0]
-
-# # Export search results to a dataframe:
-# df = idx.to_dataframe()
-
-# # To make the scatter map, we need to have the data mode available in one DataFrame column
-# # so we need to add a new column with the DATA_MODE of the PARAMETER:
-# df["variables"] = df["parameters"].apply(lambda x: x.split())
-# df["%s_DM" % param] = df.apply(lambda x: x['parameter_data_mode'][x['variables'].index(param)] if param in x['variables'] else '', axis=1)
-
-
-# # Finally plot the map:
-# fig, ax = scatter_map(df,
-#                       hue="%s_DM" % param,
-#                       cmap="data_mode",
-#                       figsize=(10,6),
-#                       markersize=2,
-#                       markeredgecolor=None,
-#                       traj=False,
-#                       set_global=False,
-#                       legend_title='%s data mode' % param
-#                     )
-# ax.set_title("Data mode for '%s' (%s)\n%s profiles from the %s" % (param_info['prefLabel'], 
-#                                                                    param, 
-#                                                                    "{:,}", 
-#                                                                    idx.convention_title)
-#             )
-# plt.show()
