d90f9c4527368e90de878a63117e7658e8f6835b
falkhazam <102944327+falkhazam@users.noreply.github.com>
2024-03-05 13:44:23 -0800

    temp_vs_timeCA

--- temp_vs_time_CA.py
+++ temp_vs_time_CA.py
@@ -1,11 +1,24 @@
 import argopy
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import dates as mdates
 import pandas as pd
+from datetime import datetime
+import time
 
 if __name__ == "__main__":
     # if getting the data from a csv file, uncomment the following 1 line :
-    df_raw = pd.read_csv('data.csv')
+    try:
+        df_raw = pd.read_csv('data_CA_2015.csv')
+    except:
+        print("Importing data...")
+        t0 = time.time()
+        ArgoSet = argopy.DataFetcher().region([-124, -116, 32, 42, 0, 5, '2015-01', '2015-12'])
+        print("Time to import : ", time.time() - t0)
+        ds = ArgoSet.data.argo.point2profile().to_dataframe()
+        print("Time to get data from DataFetcher : ", time.time() - t0)
+        ds.to_csv('data_CA_2015.csv')
+        df_raw = pd.read_csv('data_CA_2015.csv')
 
     # Remove rows where temp is None
 
@@ -13,16 +26,21 @@ if __name__ == "__main__":
 
     # Define the variables to work with:
     temp = np.array(df['TEMP'])
-    lat = np.array(df['LATITUDE'])
+    time_strings = np.array(df['TIME'])
 
-    plt.scatter(lat, temp, s=1)
+    time = np.array([datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S') for time_string in time_strings])
     plt.grid(True)
 
-    plt.title('Temperature Vs. Time')
+    plt.title('Temperature Vs. Time (2015)')
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%B'))
+
+    plt.scatter(time, temp, s=1)
+    plt.grid(True)
 
     # Adding labels to the axes
-    plt.xlabel('Time')
+    plt.xlabel('Month')
     plt.ylabel('Temperature')
 
-    plt.savefig('figures/temp_vs_time.png')
+    plt.savefig('figures/temp_vs_timeCA.png')
     plt.show()
