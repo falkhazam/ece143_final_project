2c560e91b1d4c21d361b7d09cf7b4d7424676bdc
falkhazam <102944327+falkhazam@users.noreply.github.com>
2024-03-05 13:44:40 -0800

    capable of fetching data

--- temp_vs_latitude.py
+++ temp_vs_latitude.py
@@ -1,11 +1,21 @@
-import argopy
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+import time
+import argopy
 
-if __name__ == "__main__":
-    # if getting the data from a csv file, uncomment the following 1 line :
-    df_raw = pd.read_csv('data.csv')
+def plot_temp_vs_latitude():
+
+    try :
+        df_raw = pd.read_csv('data.csv')
+    except :
+        print("Importing data...")
+        t0 = time.time()
+        ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01', '2015-02'])
+        print("Time to import : ", time.time()-t0)
+        ds = ArgoSet.data.argo.point2profile().to_dataframe()
+        print("Time to get data from DataFetcher : ", time.time()-t0)
+        ds.to_csv('data.csv')
 
     # Remove rows where temp is None
 
@@ -28,3 +38,5 @@ if __name__ == "__main__":
     plt.show()
 
 
+if __name__ == "__main__":
+    plot_temp_vs_latitude()
