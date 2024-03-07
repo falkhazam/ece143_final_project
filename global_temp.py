4aa21f953e9b932cc5c589aaaaf1a7e8bd85c1c6
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-03-07 11:32:09 -0800

    more plots

--- global_temp.py
+++ global_temp.py
@@ -16,8 +16,8 @@ import cartopy.crs as ccrs
 dates_list = [['2015-01', '2015-02'], ['2015-04', '2015-05'], ['2015-07', '2015-08'], ['2015-10', '2015-11']]
 argopy.set_options(src='erddap', dataset='phy', mode='standard')
 
-if __name__ == "__main__" :
 
+def plot_data() :
     for i, dates in enumerate(dates_list) :
 
         file = 'data'+str(i+1)+'.csv'
@@ -30,11 +30,16 @@ if __name__ == "__main__" :
             print("Importing data...")
             t0 = time.time()
             ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5] + dates)
-            print("Time to import : ", time.time()-t0)
+            split = time.time()-t0
+            print("Time to import : ", split)
+            t0 = time.time()
             ds = ArgoSet.data.argo.point2profile()
-            print("Time to get data from DataFetcher : ", time.time()-t0)
+            split = time.time()-t0
+            print("Time to get data from DataFetcher : ", split)
+            t0 = time.time()
             df = ds.to_dataframe()
-            print("Time to convert : ", time.time()-t0)
+            split = time.time()-t0
+            print("Time to convert : ", split)
             df.to_csv(file) 
 
         # Define the variables to work with:
@@ -56,4 +61,20 @@ if __name__ == "__main__" :
         gridlines = ax.gridlines(draw_labels=True) # shows lat/long
         
         plt.savefig('figures/global_temp' + str(i+1) + '.png')
+    
+    return True
         # plt.show()
+
+
+
+if __name__ == "__main__" :
+   
+   flag = False
+   while not flag :
+        try : flag = plot_data()
+        except :
+            print("Argo Server Timeout Error : retrying")
+            print("To exit, press ctrl+c 2x")
+            time.sleep(2)
+            
+   
