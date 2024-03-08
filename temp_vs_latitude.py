0c06792ab2e229da9eff230822a4047caef4aa2b
falkhazam <102944327+falkhazam@users.noreply.github.com>
2024-03-07 17:55:28 -0800

    Added comments

--- temp_vs_latitude.py
+++ temp_vs_latitude.py
@@ -4,39 +4,47 @@ import pandas as pd
 import time
 import argopy
 
-def plot_temp_vs_latitude():
 
-    try :
+def plot_temp_vs_latitude():
+    try:
         df_raw = pd.read_csv('data.csv')
-    except :
+    except:
         print("Importing data...")
         t0 = time.time()
-        ArgoSet = argopy.DataFetcher().region([-180,180,-90,90, 0, 5, '2015-01', '2015-02'])
-        print("Time to import : ", time.time()-t0)
+        ArgoSet = argopy.DataFetcher().region([-180, 180, -90, 90, 0, 5, '2015-01', '2015-02'])
+        print("Time to import : ", time.time() - t0)
         ds = ArgoSet.data.argo.point2profile().to_dataframe()
-        print("Time to get data from DataFetcher : ", time.time()-t0)
+        print("Time to get data from DataFetcher : ", time.time() - t0)
         ds.to_csv('data.csv')
 
     # Remove rows where temp is None
-
     df = df_raw.dropna(subset=['TEMP'])
 
     # Define the variables to work with:
     temp = np.array(df['TEMP'])
     lat = np.array(df['LATITUDE'])
 
+    # Scatter data and add grid
     plt.scatter(lat, temp, s=1)
     plt.grid(True)
 
-    plt.title('Temperature Vs. Latitude')
-
-    # Adding labels to the axes
+    # Adding labels to the axes and title
     plt.xlabel('Latitude')
     plt.ylabel('Temperature')
+    plt.title('Temperature Vs. Latitude')
 
     plt.savefig('figures/temp_vs_latitude.png')
     plt.show()
 
+    return True
+
 
 if __name__ == "__main__":
-    plot_temp_vs_latitude()
+    flag = False
+    while not flag:
+        try:
+            flag = plot_temp_vs_latitude()
+        except:
+            print("Argo Server Timeout Error : retrying")
+            print("To exit, press ctrl+c 2x")
+            time.sleep(2)
--- temp_vs_time_CA.py
+++ temp_vs_time_CA.py
@@ -3,11 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 import pandas as pd
-from datetime import datetime
 import time
+from datetime import datetime
 
-if __name__ == "__main__":
-    # if getting the data from a csv file, uncomment the following 1 line :
+
+def plot_temp_vs_time_CA():
+    # Fetch data for California in 2015
     try:
         df_raw = pd.read_csv('data_CA_2015.csv')
     except:
@@ -21,26 +22,39 @@ if __name__ == "__main__":
         df_raw = pd.read_csv('data_CA_2015.csv')
 
     # Remove rows where temp is None
-
     df = df_raw.dropna(subset=['TEMP'])
 
     # Define the variables to work with:
     temp = np.array(df['TEMP'])
     time_strings = np.array(df['TIME'])
 
+    # Assigns each datetime formatted time string to an array of datetime objects
     time = np.array([datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S') for time_string in time_strings])
-    plt.grid(True)
 
-    plt.title('Temperature Vs. Time (California, 2015)')
-
+    # Plots month name in x-axis
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%B'))
 
+    # Scatter data and add grid
     plt.scatter(time, temp, s=1)
     plt.grid(True)
 
-    # Adding labels to the axes
+    # Title and labels
     plt.xlabel('Month')
     plt.ylabel('Temperature')
+    plt.title('Temperature Vs. Time (California, 2015)')
 
+    # Save figure and show the plot
     plt.savefig('figures/temp_vs_timeCA.png')
     plt.show()
+    return True
+
+
+if __name__ == "__main__":
+    flag = False
+    while not flag:
+        try:
+            flag = plot_temp_vs_time_CA()
+        except:
+            print("Argo Server Timeout Error : retrying")
+            print("To exit, press ctrl+c 2x")
+            time.sleep(2)
